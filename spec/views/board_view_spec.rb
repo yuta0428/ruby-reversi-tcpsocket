d80ed04c56646267ec8edd912c46ec61e2603a9d
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2020-06-06 13:57:30 +0900

    add spec

--- spec/views/board_view_spec.rb
+++ spec/views/board_view_spec.rb
@@ -17,21 +17,28 @@ describe BoardView do
   it 'is valid render' do
     v = <<-EOS.chomp
    0 1 2 3 4 5
- 0#{ [wa,wa,wa,wa,wa,wa].join }
- 1#{ [wa,em,em,em,em,wa].join }
- 2#{ [wa,em,pw,pb,em,wa].join }
- 3#{ [wa,em,pb,pw,em,wa].join }
- 4#{ [wa,em,em,em,em,wa].join }
- 5#{ [wa,wa,wa,wa,wa,wa].join }
+ 0#{[wa, wa, wa, wa, wa, wa].join}
+ 1#{[wa, em, em, em, em, wa].join}
+ 2#{[wa, em, pw, pb, em, wa].join}
+ 3#{[wa, em, pb, pw, em, wa].join}
+ 4#{[wa, em, em, em, em, wa].join}
+ 5#{[wa, wa, wa, wa, wa, wa].join}
     EOS
     l = [
-      -1,-1,-1,-1,-1,-1,
-      -1, 0, 0, 0, 0,-1,
-      -1, 0, 1, 2, 0,-1,
-      -1, 0, 2, 1, 0,-1,
-      -1, 0, 0, 0, 0,-1,
-      -1,-1,-1,-1,-1,-1,
+      -1, -1, -1, -1, -1, -1,
+      -1, 0, 0, 0, 0, -1,
+      -1, 0, 1, 2, 0, -1,
+      -1, 0, 2, 1, 0, -1,
+      -1, 0, 0, 0, 0, -1,
+      -1, -1, -1, -1, -1, -1
     ]
     expect(@obj.render(l)).to eq v
+  end
+
+  it 'is valid type2mark' do
+    expect(@obj.type2mark(-1)).to eq wa
+    expect(@obj.type2mark(0)).to eq em
+    expect(@obj.type2mark(1)).to eq pw
+    expect(@obj.type2mark(2)).to eq pb
   end
 end
